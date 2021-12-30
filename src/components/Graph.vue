1f4299291ea35f827000516226860fcb8e1d5e4d
Qiuxia Fan <fine0830@outlook.com>
2021-12-30 16:37:12 +0800

    feat: create Graph Component with echarts

---
+++ src/components/Graph.vue
@@ -0,0 +1,73 @@
+<!-- Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. -->
+<template>
+  <div
+    class="echarts"
+    ref="chart"
+    :style="`height:${height};width:${width};`"
+  ></div>
+</template>
+<script lang="ts" setup>
+import {
+  onMounted,
+  watch,
+  reactive,
+  ref,
+  defineProps,
+  onBeforeUnmount,
+} from "vue";
+import type { PropType } from "vue";
+import * as echarts from "echarts";
+const dom = ref<any>(null);
+const state = reactive<any>({
+  instanceChart: null,
+});
+const props = defineProps({
+  clickEvent: { type: Function as PropType<(param: unknown) => void> },
+  height: { type: Number, default: 100 },
+  width: { type: Number, default: 300 },
+  option: { type: Object as PropType<any>, default: () => ({}) },
+});
+onMounted(() => {
+  drawEcharts();
+  window.addEventListener("resize", state.instanceChart.resize);
+});
+function drawEcharts(): void {
+  state.instanceChart = echarts.init(dom.value, "");
+  state.instanceChart.setOption(state.option);
+  state.instanceChart.on("click", (params: any) => {
+    if (!props.clickEvent) {
+      return;
+    }
+    props.clickEvent(params);
+  });
+}
+watch(
+  () => props.option,
+  (opt) => {
+    if (state.instanceChart) {
+      state.instanceChart.setOption(opt, true);
+    } else {
+      drawEcharts();
+    }
+  }
+);
+onBeforeUnmount(() => {
+  if (state.instanceChart.dispose) {
+    state.instanceChart.dispose();
+  }
+  window.removeEventListener("resize", state.instanceChart.resize);
+});
+</script>
