e1d5095d75c5778208b4f3318542221b31967442
Qiuxia Fan <fine0830@outlook.com>
2022-01-04 16:01:53 +0800

    build: update eslint rules

--- src/views/dashboard/graphs/InstanceList.vue
+++ src/views/dashboard/graphs/InstanceList.vue
@@ -13,9 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. -->
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
+  <el-table :data="data" style="width: 100%">
+    <el-table-column prop="label" label="Service Instances" />
   </el-table>
 </template>
 <script setup lang="ts">
@@ -30,26 +29,4 @@ defineProps({
   },
   theme: { type: String, default: "" },
 });
-const tableData = [
-  {
-    date: "2016-05-03",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-02",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-04",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-01",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-];
 </script>
