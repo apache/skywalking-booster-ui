0f5c69a496fba44225b7ecb8b5dd5fefe797578a
Fine <fanxue0830@gmail.com>
2022-08-15 14:40:38 +0800

    update text

--- src/views/dashboard/configuration/TimeRange.vue
+++ src/views/dashboard/configuration/TimeRange.vue
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License. -->
 <template>
   <div class="item">
-    <span class="label">{{ t("content") }}</span>
+    <span class="label">{{ t("text") }}</span>
     <el-input
       class="input"
       v-model="text"
