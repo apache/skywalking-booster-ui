54ace115245b3f7876072b98421a6e6fe80ac5ae
pg.yang <pg.yang@hotmail.com>
2022-12-06 20:26:23 +0800

    unify AWSCloud name

--- src/locales/lang/es.ts
+++ src/locales/lang/es.ts
@@ -378,7 +378,7 @@ const msg = {
   language: "Lenguaje",
   gateway: "Puerta",
   virtualMQ: "MQ virtual",
-  AWSCloud: "Amazon Cloud",
+  AWSCloud: "AWS Cloud",
   AWSCloudEKS: "EKS"
 };
 export default msg;
