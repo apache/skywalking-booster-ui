746a71ac102d01502e8d90c8530182401287f6ba
Fine <fanxue0830@gmail.com>
2023-01-14 16:12:30 +0800

    feat: delete filters

--- src/views/dashboard/List.vue
+++ src/views/dashboard/List.vue
@@ -226,6 +226,7 @@ limitations under the License. -->
       standard?: unknown;
       label?: string;
       value?: string;
+      filters?: unknown;
     })[],
   ) {
     for (const child of children || []) {
@@ -235,6 +236,7 @@ limitations under the License. -->
       delete child.id;
       delete child.label;
       delete child.value;
+      delete child.filters;
       if (isEmptyObject(child.graph)) {
         delete child.graph;
       }
