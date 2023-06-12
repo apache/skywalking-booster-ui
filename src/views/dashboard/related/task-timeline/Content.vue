66126a36a96db7833d4b336a94ef85b823495066
Fine <fanxue0830@gmail.com>
2023-06-12 17:39:16 +0800

    fix: height

--- src/views/dashboard/related/task-timeline/Content.vue
+++ src/views/dashboard/related/task-timeline/Content.vue
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. -->
 <template>
-  <div class="flex-v content">
+  <div class="content">
     <Timeline />
     <ProfilingPanel :config="config" />
   </div>
