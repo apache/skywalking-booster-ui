decbda52d6484255266a241e2cc261a665dd0c27
Fine <fanxue0830@gmail.com>
2023-11-09 15:16:21 +0800

    feat: update toplist

--- src/views/dashboard/graphs/TopList.vue
+++ src/views/dashboard/graphs/TopList.vue
@@ -201,7 +201,7 @@ limitations under the License. -->
 
     &:hover {
       color: $active-color;
-      background-color: #eee;
+      background-color: $popper-hover-bg-color;
     }
   }
 </style>
