5122dcda85c2712846bf90d5664fdd6c5e222518
Fine <fanxue0830@gmail.com>
2023-11-17 12:51:32 +0800

    fix: update

--- src/views/dashboard/related/log/LogTable/LogService.vue
+++ src/views/dashboard/related/log/LogTable/LogService.vue
@@ -25,7 +25,7 @@ limitations under the License. -->
         {{ dateFormat(data.timestamp) }}
       </span>
       <span v-else-if="item.label === 'tags'" :class="level.toLowerCase()"> > </span>
-      <el-tooltip v-else-if="item.label === 'traceId'" content="Trace Link">
+      <el-tooltip v-else-if="item.label === 'traceId' && !noLink" content="Trace Link">
         <span :class="noLink ? '' : 'blue'">
           <Icon v-if="data[item.label]" iconName="merge" />
         </span>
