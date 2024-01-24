859a0b75a0524fd0857da3e9086885dc2db58093
Fine <fanxue0830@gmail.com>
2024-01-24 16:03:32 +0800

    feat: update

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -13,7 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. -->
 <template>
-  <div class="not-found flex-h"> Page Not Found </div>
+  <div class="not-found flex-h">
+    <Icon size="largest" iconName="logo-light" />
+    <h1 class="ml-20">404 Page Not Found</h1>
+  </div>
 </template>
 <style lang="scss" scoped>
   .not-found {
@@ -21,6 +24,10 @@ limitations under the License. -->
     width: 100%;
     align-items: center;
     justify-content: center;
-    font-size: var(--font-size-bigger);
+  }
+
+  .icon {
+    width: 160px;
+    height: 160px;
   }
 </style>
